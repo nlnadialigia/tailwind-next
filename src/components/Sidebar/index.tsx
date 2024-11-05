69ae2b2edd7d72db9c566ad9bedc1e401b632675
Nadia <nlnadialigia@gmail.com>
2024-11-04 22:47:57 -0300

    criação do componente de input

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -9,6 +9,7 @@ import {
   SquareStack,
   Users,
 } from 'lucide-react'
+import * as Input from '../Input'
 import { Logo } from './Logo'
 import { NavItem } from './NavItem'
 import { Profile } from './Profile'
@@ -19,14 +20,12 @@ export default function Sidebar() {
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="item-center mx-1 flex w-full gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 flex-shrink-0 text-zinc-500" />
-
-        <input
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-          placeholder="Search"
-        />
-      </div>
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 flex-shrink-0 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control />
+      </Input.Root>
 
       <nav className="space-y-0.5">
         <NavItem href="/" title="Home" icon={Home} />

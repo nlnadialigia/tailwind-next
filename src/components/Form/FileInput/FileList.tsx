6e56c71d3bf36fef8bf18a97f011bdf24bb4a56e
Nadia <nlnadialigia@gmail.com>
2024-11-08 16:25:07 -0300

    animando portfolio

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import { formatBytes } from '@/src/utils/format-bytes'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { Trash2, UploadCloud } from 'lucide-react'
 import { useFileInput } from './Root'
 
 export function FileList() {
   const { files } = useFileInput()
+  const [parent] = useAutoAnimate()
 
   return (
-    <div className="mt-4 space-y-3">
+    <div className="mt-4 space-y-3" ref={parent}>
       {files.map((file) => {
         return (
           <div

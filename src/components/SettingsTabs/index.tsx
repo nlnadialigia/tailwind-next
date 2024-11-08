16649557fbaf19548ece8dceeabd317db8c7945d
Nadia <nlnadialigia@gmail.com>
2024-11-08 20:31:42 -0300

    responsividade das abas

--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import * as ScrollArea from '@radix-ui/react-scroll-area'
 import * as Tabs from '@radix-ui/react-tabs'
 import { useState } from 'react'
 import { TabItem } from './TabItem'
@@ -22,16 +23,27 @@ export function SettingsTabs() {
 
   return (
     <Tabs.Root value={currentTab} onValueChange={setCurrentTab}>
-      <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
-        {settingsTabs.map(({ value, title }) => (
-          <TabItem
-            key={value}
-            value={value}
-            title={title}
-            isSelected={currentTab === value}
-          />
-        ))}
-      </Tabs.List>
+      <ScrollArea.Root className="w-full" type="scroll">
+        <ScrollArea.Viewport className="w-full overflow-x-scroll">
+          <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
+            {settingsTabs.map(({ value, title }) => (
+              <TabItem
+                key={value}
+                value={value}
+                title={title}
+                isSelected={currentTab === value}
+              />
+            ))}
+          </Tabs.List>
+        </ScrollArea.Viewport>
+
+        <ScrollArea.Scrollbar
+          orientation="horizontal"
+          className="flex h-0.5 translate-y-1.5 touch-none select-none flex-col bg-zinc-100"
+        >
+          <ScrollArea.Thumb className="relative flex-1 rounded-full bg-zinc-300" />
+        </ScrollArea.Scrollbar>
+      </ScrollArea.Root>
     </Tabs.Root>
   )
 }

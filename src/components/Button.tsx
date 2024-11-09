5db495b40936deb6974d58a1a44f47cd93abf649
Nadia <nlnadialigia@gmail.com>
2024-11-08 21:22:20 -0300

    tema dark da sidebar

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,8 @@ const buttonVariant = tv({
     variant: {
       primary: 'bg-violet-700 text-violet-50 hover:bg-violet-600',
       outline: 'border border-zinc-300 text-zinc-700 hover:bg-zinc-50',
-      ghost: 'rounded-md px-2 houver:bg-zinc-50 shadow-none text-zinc-500',
+      ghost:
+        'rounded-md px-2 houver:bg-zinc-50 shadow-none text-zinc-500 dark:hover:bg-zinc-800 dark:text-zinc-400',
     },
   },
 
--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -10,13 +10,15 @@ export function Profile() {
         className="h-10 w-10 rounded-full"
       />
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">Nadia Ligia</span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
+          Nadia Ligia
+        </span>
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           nlnadialigia@gmail.com
         </span>
       </div>
       <Button type="button" variant="ghost">
-        <LogOut className="h-5 w-5 text-zinc-500" />
+        <LogOut className="h-5 w-5" />
       </Button>
     </div>
   )
